6fe6b7316239bb4d35eac24179b68d198bb4af50
cipeng <fmulkhan@gmail.com>
2025-09-13 10:29:29 +0800

    First Commit

---
+++ src/app/api/radius/users/route.ts
@@ -0,0 +1,77 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+interface PostRequestBody {
+    username: string;
+    password: string;
+    groupname?: string;
+}
+
+
+let defaultGroupCache : string   | null = null;
+
+// async function getDefaultGroup(): Promise<string | null> {
+
+//     if (defaultGroupCache){
+//         return defaultGroupCache;
+
+//     }
+
+//     const defaultGroupSetting = await prisma.radgroupreply.findFirst({
+        
+//     })
+    
+// }
+
+export async function POST(req: NextRequest) {
+
+    try{
+        const { username, password, groupname } = await req.json() as PostRequestBody;
+        if (!username || !password) {
+            return NextResponse.json({ message: 'Username dan password harus diisi' }, { status: 400 });
+        }
+
+
+
+        const result = await prisma.$transaction([
+            prisma.radcheck.create({
+                data: {
+                    username: username,
+                    attribute: 'Cleartext-Password',
+                    op: ':=',
+                    value: password,
+                },
+            }), 
+            prisma.radusergroup.create({
+                data: {
+                    username: username,
+                    groupname: groupname,
+                },
+            }),
+        ]);
+        return NextResponse.json({message: 'User berhasil dibuat',result}, {status: 200});
+    
+    } catch (error: any) {
+        return NextResponse.json({ message: 'Maaf terjadi kesalahan pada server',error: error.message }, { status: 500 });
+    }
+}
+
+export async function GET() {
+
+    try{
+        const users = await prisma.radcheck.findMany({
+            select: {
+                username: true,
+            },
+            distinct: ['username'],
+            orderBy: {
+                username: 'asc',
+            }
+        });
+
+        return NextResponse.json({users}, {status: 200});
+    }catch (error: any) {
+        return NextResponse.json({ message: 'Maaf terjadi kesalahan pada server',error: error.message }, { status: 500 });
+    }
+    
+}
