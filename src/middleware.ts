b8c616db2dc3be5c8e0cc22f9b74d4fe72182060
cipeng <fmulkhan@gmail.com>
2025-10-15 11:33:03 +0800

    Config eslint

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,24 @@
-export { default } from "next-auth/middleware";
+// middleware.ts
+
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login", // Pastikan ini adalah halaman login Anda
+  },
+});
+
+// Konfigurasi matcher yang sudah diperbaiki
 export const config = {
-     matcher: [
-        "/",
-        "/api/radius/:path*",
-    ] 
-    
+  matcher: [
+    /*
+     * Cocokkan semua path, KECUALI yang dimulai dengan:
+     * - api (rute API)
+     * - _next/static (file statis)
+     * - _next/image (file optimasi gambar)
+     * - favicon.ico (file ikon)
+     * - login (halaman login Anda)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|login).*)',
+  ],
 };
